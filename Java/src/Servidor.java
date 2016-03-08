7f9ef51e61e97862c73a3e049ec3e7f4901f9062
Tiago Araujo <taraujodesouza@gmail.com>
2016-03-08 18:18:47 -0300

    Removido o antigo projeto Java

--- Java/src/Servidor.java
+++
@@ -1,7 +0,0 @@
-
-import java.rmi.*;
-
-public interface Servidor extends Remote {
-    //métodos do servidor que podem ser acessados remotamente
-    public boolean validaChave(String mensagem) throws RemoteException;
-}
