79123bf280ea3aef6781f6b79fd45fc22bf2eeb8
Tiago Araujo <taraujodesouza@gmail.com>
2016-03-08 22:30:05 -0300

    Versão inicial do servidor Bluetooth Java

---
+++ Java/serverBluetooth/src/serverbluetooth/ProcessConnectionThread.java
@@ -0,0 +1,71 @@
+package serverbluetooth;
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.InputStream;
+
+import javax.microedition.io.StreamConnection;
+
+public class ProcessConnectionThread implements Runnable {
+
+    private StreamConnection mConnection;
+    // Constant that indicate command from devices
+    private static final int EXIT_CMD = -1;
+    private static final int KEY_RIGHT = 1;
+    private static final int KEY_LEFT = 2;
+
+    public ProcessConnectionThread(StreamConnection connection) {
+        mConnection = connection;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            // prepare to receive data
+            InputStream inputStream = mConnection.openInputStream();
+
+            System.out.println("Aguardando input...");
+
+            while (true) {
+                int command = inputStream.read();
+
+                if (command == EXIT_CMD) {
+                    System.out.println("Processo Finalizado!");
+                    break;
+                }
+
+                processCommand(command);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Process the command from client
+     *
+     * @param command the command code
+     */
+    private void processCommand(int command) {
+        try {
+            Robot robot = new Robot();
+            switch (command) {
+                case KEY_RIGHT:
+                    robot.keyPress(KeyEvent.VK_RIGHT);
+                    System.out.println("Direita");
+                    // release the key after it is pressed. Otherwise the event just keeps getting trigged	    		
+                    robot.keyRelease(KeyEvent.VK_RIGHT);
+                    break;
+                case KEY_LEFT:
+                    robot.keyPress(KeyEvent.VK_LEFT);
+                    System.out.println("Esquerda");
+                    // release the key after it is pressed. Otherwise the event just keeps getting trigged	    		
+                    robot.keyRelease(KeyEvent.VK_LEFT);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Java/serverBluetooth/src/serverbluetooth/RemoteBluetoothServer.java
@@ -0,0 +1,9 @@
+package serverbluetooth;
+
+public class RemoteBluetoothServer {
+
+    public static void main(String[] args) {
+        Thread waitThread = new Thread(new WaitThread());
+        waitThread.start();
+    }
+}
