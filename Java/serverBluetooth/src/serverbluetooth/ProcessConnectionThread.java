2b0630bb2343e7edbe584e1318a648a329ab49ab
Tiago Araujo <taraujodesouza@gmail.com>
2016-03-15 00:24:33 -0300

    Java - Implementada interface de status da conexão (Elias)

--- Java/serverBluetooth/src/serverbluetooth/ProcessConnectionThread.java
+++ Java/serverBluetooth/src/serverbluetooth/ProcessConnectionThread.java
@@ -7,6 +7,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import javax.microedition.io.StreamConnection;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class ProcessConnectionThread implements Runnable {
     
@@ -16,9 +18,41 @@ public class ProcessConnectionThread implements Runnable {
     private static final int EXIT_CMD = -1;
     private static final int ACESSO_LIBERADO = 1;
     private static final int ACESSO_NEGADO = 2;
-
+        
+    public ProcessConnectionThread(StreamConnection connection, JLabel jStatus, JLabel jVisor) {
+        mConnection = connection;
+        this.jStatus = jStatus;
+        this.jVisor = jVisor;
+    }
+    
     public ProcessConnectionThread(StreamConnection connection) {
         mConnection = connection;
+    }
+    
+    private JLabel jStatus, jVisor;
+
+    public StreamConnection getmConnection() {
+        return mConnection;
+    }
+
+    public void setmConnection(StreamConnection mConnection) {
+        this.mConnection = mConnection;
+    }
+
+    public JLabel getjVisor() {
+        return jVisor;
+    }
+
+    public void setjVisor(JLabel jVisor) {
+        this.jVisor = jVisor;
+    }
+
+    public JLabel getjStatus() {
+        return jStatus;
+    }
+
+    public void setjStatus(JLabel jStatus) {
+        this.jStatus = jStatus;
     }
 
     @Override
@@ -27,7 +61,9 @@ public class ProcessConnectionThread implements Runnable {
             // recebe os dados via stream
             InputStream inputStream = mConnection.openInputStream();
 
+            jStatus.setText("Conexão estabelecida!");
             System.out.println("Conexão estabelecida!");
+            jStatus.setText("Aguardando input...");
             System.out.println("Aguardando input...");
 
             while (true) {
@@ -35,6 +71,7 @@ public class ProcessConnectionThread implements Runnable {
                 int command = inputStream.read();
 
                 if (command == EXIT_CMD) {
+                    jStatus.setText("Processo Finalizado!");
                     System.out.println("Processo Finalizado!");
                     break;
                 }
@@ -47,15 +84,33 @@ public class ProcessConnectionThread implements Runnable {
     }
 
     private void processCommand(int command) {
+        
         try {
+            
+            String imageName = "/serverbluetooth/cinza.png";
+            ImageIcon icon = new ImageIcon(imageName);
+            jVisor.setText("Nenhum Dispositivo Conectado");
+            
             switch (command) {
                 case ACESSO_LIBERADO:
                     System.out.println("Acesso Liberado!");
+                    imageName = "/serverbluetooth/verde.png";
+                    icon = new ImageIcon(imageName);
+                    jVisor.setText("Acesso Liberado");
+                    icon.getImage().flush();
+            jVisor.setIcon( icon );
                     break;
                 case ACESSO_NEGADO:
                     System.out.println("Acesso Negado!");
-                    break;
+                    imageName = "/serverbluetooth/vermelho.png";
+                    icon = new ImageIcon(imageName);
+                    jVisor.setText("Acesso Negado");
+                    break;             
             }
+            
+            icon.getImage().flush();
+            jVisor.setIcon( icon );
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ Java/serverBluetooth/src/serverbluetooth/TelaServidor.java
@@ -0,0 +1,120 @@
+
+package serverbluetooth;
+
+
+public class TelaServidor extends javax.swing.JFrame {
+
+    
+    public TelaServidor() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        waitThread = new Thread(new WaitThread(jStatus, jVisor, jName));
+        waitThread.start();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jStatus = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jName = new javax.swing.JLabel();
+        jVisor = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jStatus.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setText("Nome do Dispositivo:");
+
+        jName.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jName.setForeground(new java.awt.Color(102, 51, 255));
+
+        jVisor.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jVisor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/serverbluetooth/cinza.png"))); // NOI18N
+        jVisor.setText("Nenhum Dispositivo Conectado");
+        jVisor.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        jVisor.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jVisor.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(42, 42, 42)
+                        .addComponent(jStatus))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jName)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 301, Short.MAX_VALUE)
+                .addComponent(jVisor)
+                .addGap(282, 282, 282))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jName))
+                .addGap(98, 98, 98)
+                .addComponent(jVisor)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 157, Short.MAX_VALUE)
+                .addComponent(jStatus)
+                .addGap(38, 38, 38))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaServidor().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jName;
+    private javax.swing.JLabel jStatus;
+    private javax.swing.JLabel jVisor;
+    // End of variables declaration//GEN-END:variables
+    Thread waitThread;
+}
