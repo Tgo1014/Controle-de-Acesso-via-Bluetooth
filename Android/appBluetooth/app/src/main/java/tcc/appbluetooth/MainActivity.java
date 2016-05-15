92b14129c57af1d5177c788b7ddc5b90289bb3cf
Tiago Araujo <taraujodesouza@gmail.com>
2016-05-15 19:29:49 -0300

    Commit Inicial

--- Android/appBluetooth/app/src/main/java/tcc/appbluetooth/MainActivity.java
+++ Android/appBluetooth/app/src/main/java/tcc/appbluetooth/MainActivity.java
@@ -3,7 +3,9 @@ package tcc.appbluetooth;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
@@ -12,12 +14,23 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //variável para solicitar a ativacao, caso o bluetooth esteja desativado
     private final static int REQUEST_ENABLE_BT = 1;
+    private final static int REQUEST_FILE = 2;
     String status;
     Smartphone user = new Smartphone();
+
+    //responsável por salvar o caminho do certificado, mesmo que a aplicação seja encerrada
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +39,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         TextView txtID = (TextView) findViewById(R.id.txtID);
         TextView txtSIM = (TextView) findViewById(R.id.txtSIM);
+        TextView txtStatusCertificado = (TextView) findViewById(R.id.txtStatusCertificado);
         Button btnBuscarDevice = (Button) findViewById(R.id.btnBuscarDevice);
+        Button btnSelecionarCertificado = (Button) findViewById(R.id.btnSelecionaCertificado);
+
         btnBuscarDevice.setOnClickListener(this);
+        btnSelecionarCertificado.setOnClickListener(this);
 
         //pega os dados de telefonia do aparelho
         TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
@@ -43,6 +60,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         txtSIM.setText("SIM_ID: " + tm.getSimSerialNumber());
         user.setSIM_ID(tm.getSimSerialNumber());
+
+        //requisita as variáveis que são guardadas mesmo que a aplicação seja encerrada
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("CAMINHO_CERTIFICADO",  MODE_PRIVATE);
+
+        //Verifica se algum certificado já foi selecionado anteriormente
+        String caminho = pref.getString("CAMINHO_CERTIFICADO", null);
+        if (caminho == null){
+            txtStatusCertificado.setText("Status: Certificado não selecionado");
+        } else {
+            txtStatusCertificado.setText("Status: Certificado OK");
+        }
     }
 
 
@@ -69,6 +97,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             }
+
+            case R.id.btnSelecionaCertificado: {
+
+                //chama biblioteca de explorer de arquivos
+                Intent intent = new Intent(this, FilePickerActivity.class);
+                //filtra apenas arquivos .cer
+                intent.putExtra(FilePickerActivity.ARG_FILE_FILTER, Pattern.compile(".*\\.cer"));
+                intent.putExtra(FilePickerActivity.ARG_DIRECTORIES_FILTER, false);
+                intent.putExtra(FilePickerActivity.ARG_SHOW_HIDDEN, false);
+                startActivityForResult(intent, REQUEST_FILE);
+
+
+                break;
+            }
         }
     }
 
@@ -82,6 +124,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(this, ListarDispositivosActivity.class));
             }else{
                 Toast.makeText(MainActivity.this, "Está aplicação necessita do Bluetooth ativo para funcionar", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        if (requestCode == REQUEST_FILE) {
+            if (resultCode == RESULT_OK) {
+                String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+                // Do anything with file
+            } else {
+                Toast.makeText(MainActivity.this, "Cancelado", Toast.LENGTH_LONG).show();
             }
         }
 
